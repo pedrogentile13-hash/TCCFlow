88b25f7d7e582a227ab3783bb09853d939bed98b
Pedro Gentile <pedrogentile13@gmail.com>
2026-03-08 16:19:10 -0300

    Refactor Firebase initialization and add config check
    Updated Firebase initialization to use Firebase SDK methods and added a configuration check function.

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// ============================================================
+// TCCFlow - Firebase Configuration
+// ============================================================
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBOiwIkkFyrWhemD6cGl3A85Hvlz16NU5o",
-  authDomain: "tccflow-94be6.firebaseapp.com",
-  projectId: "tccflow-94be6",
-  storageBucket: "tccflow-94be6.firebasestorage.app",
-  messagingSenderId: "207188280509",
-  appId: "1:207188280509:web:34dc672554887295c3e3d4",
-  measurementId: "G-VR3R2TD099"
+    apiKey: "AIzaSyBOiwIkkFyrWhemD6cGl3A85Hvlz16NU5o",
+    authDomain: "tccflow-94be6.firebaseapp.com",
+    projectId: "tccflow-94be6",
+    storageBucket: "tccflow-94be6.firebasestorage.app",
+    messagingSenderId: "207188280509",
+    appId: "1:207188280509:web:34dc672554887295c3e3d4"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Check if Firebase is configured
+function isFirebaseConfigured() {
+    return firebaseConfig.apiKey !== "SUA_API_KEY_AQUI";
+}
